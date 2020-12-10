08cc124a3414f1c8083ab9c88376952aa102e408
moranzcw <moranzcw@gmail.com>
2020-12-10 13:38:09 +0800

    feat: filling triangles with depth testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "geometry.h"
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <limits>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -45,106 +47,75 @@ void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color){
     line(t0.x, t0.y, t1.x, t1.y, image, color);
 }
 
-// 画出三角形轮廓
-void triangle1(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-    line(t0, t1, image, color); 
-    line(t1, t2, image, color); 
-    line(t2, t0, image, color); 
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
-// 画出三角形轮廓
-void triangle2(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-    // 按y坐标从低到高排序
-    if (t0.y>t1.y) std::swap(t0, t1); 
-    if (t0.y>t2.y) std::swap(t0, t2); 
-    if (t1.y>t2.y) std::swap(t1, t2);
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    //  计算包围盒
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+        }
+    }
 
-    line(t0, t1, image, green); 
-    line(t1, t2, image, green); 
-    line(t2, t0, image, red); 
+    // 枚举包围盒内每一点，检测是否在三角形中，并进行深度测试
+    Vec3f P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+            // 如果点在三角形外，则丢弃
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0)
+                continue;
+            // 计算该点深度
+            P.z = 0;
+            for (int i=0; i<3; i++)
+                P.z += pts[i][2]*bc_screen[i];
+            // 进行深度测试
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
 }
 
-// 填充三角形
-void triangle3(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-    // 按y坐标从低到高排序
-    if (t0.y>t1.y) std::swap(t0, t1); 
-    if (t0.y>t2.y) std::swap(t0, t2); 
-    if (t1.y>t2.y) std::swap(t1, t2); 
-
-    // 画出三角形的下半部分
-    int total_height = t2.y-t0.y; 
-    for (int y=t0.y; y<=t1.y; y++) { 
-        int segment_height = t1.y-t0.y+1; // +1可以让其不为零，但会导致segment_height偏大
-        float alpha = (float)(y-t0.y)/total_height; // 可能产生除零错误
-        float beta  = (float)(y-t0.y)/segment_height; // 注意不要除零错误
-        Vec2i A = t0 + (t2-t0)*alpha; 
-        Vec2i B = t0 + (t1-t0)*beta; 
-        if (A.x>B.x) std::swap(A, B); 
-        for (int j=A.x; j<=B.x; j++) { 
-            image.set(j, y, color); // 注意int转化 t0.y+i != A.y 
-        } 
-    } 
-    // 画出三角形的上半部分
-    for (int y=t1.y; y<=t2.y; y++) { 
-        int segment_height =  t2.y-t1.y+1; // +1可以让其不为零，但会导致segment_height偏大
-        float alpha = (float)(y-t0.y)/total_height; // 可能产生除零错误
-        float beta  = (float)(y-t1.y)/segment_height; // 注意不要除零错误
-        Vec2i A = t0 + (t2-t0)*alpha; 
-        Vec2i B = t1 + (t2-t1)*beta; 
-        if (A.x>B.x) std::swap(A, B); 
-        for (int j=A.x; j<=B.x; j++) { 
-            image.set(j, y, color); // 注意int转化 t0.y+i != A.y 
-        } 
-    } 
-}
-
-// 填充三角形
-void triangle4(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-    if (t0.y==t1.y && t0.y==t2.y) return; // 忽略三点y坐标相等的三角形，避免后面的total_height为零
-    // 按y坐标从低到高排序
-    if (t0.y>t1.y) std::swap(t0, t1); 
-    if (t0.y>t2.y) std::swap(t0, t2); 
-    if (t1.y>t2.y) std::swap(t1, t2); 
-
-    int total_height = t2.y-t0.y; 
-    for (int i=0; i<total_height; i++) { 
-        bool upper_half = i>t1.y-t0.y || t1.y==t0.y; // 前一条件若t1==t2时，则跳过上半部分，后一条件若t1.y==t0.y时，则跳过下半部分，解决除零错误
-        int segment_height = upper_half ? t2.y-t1.y : t1.y-t0.y; 
-        float alpha = (float)i/total_height; 
-        float beta  = (float)(i-(upper_half ? t1.y-t0.y : 0))/segment_height; // 在上述条件下，这里不会有除零错误 
-        Vec2i A =               t0 + (t2-t0)*alpha; 
-        Vec2i B = upper_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta; 
-        if (A.x>B.x) std::swap(A, B); 
-        for (int j=A.x; j<=B.x; j++) { 
-            image.set(j, t0.y+i, color); // 注意int转化 t0.y+i != A.y 
-        } 
-    } 
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
 }
 
 int main(int argc, char** argv) {
-    TGAImage image(width, height, TGAImage::RGB);
-    Model *model = new Model("african_head.obj");
-    Vec3f light_dir(0,0,-1);
+    Model *model = new Model("obj/african_head.obj");
+    
+    float *zbuffer = new float[width*height];
+    for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
+    TGAImage image(width, height, TGAImage::RGB);
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j=0; j<3; j++) {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
-            world_coords[j]  = v;
-        }
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
-        n.normalize();
-        float intensity = n*light_dir;
-        if (intensity>0) {
-            triangle4(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
-        }
+        Vec3f pts[3];
+        for (int i=0; i<3; i++)
+            pts[i] = world2screen(model->vert(face[i]));
+        triangle(pts, zbuffer, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
-    delete model;
+
     image.flip_vertically(); // 上下翻转，让坐标原点在左下角
     image.write_tga_file("output.tga");
+
+    delete model;
 
     return 0;
 }
