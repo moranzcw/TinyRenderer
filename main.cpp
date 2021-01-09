a1ec6da24a46aa8821ebe7b1acb281de1f493b1f
moranzcw <moranzcw@gmail.com>
2021-01-09 16:44:41 +0800

    feat: add a shadow shader

--- main.cpp
+++ main.cpp
@@ -7,17 +7,19 @@
 #include "our_gl.h"
 
 Model *model = NULL;
-const int width = 1200;
-const int height = 1200;
+float *shadowbuffer = NULL;
+
+const int width = 1600;
+const int height = 1600;
 
 Vec3f light_dir(1, 1, 1);
-Vec3f eye(1, 1, 3);
+Vec3f eye(1, 1, 4);
 Vec3f center(0, 0, 0);
 Vec3f up(0, 1, 0);
 
 struct GouraudShader : public IShader
 {
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     Vec3f varying_intensity;      // 顶点着色器写入，片段着色器读取
 
     // iface为面的序号，nthvert为顶点序号
@@ -25,8 +27,8 @@ struct GouraudShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = Projection * ModelView * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 计算光照强度
         varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir);
         return gl_Vertex;
@@ -44,7 +46,7 @@ struct GouraudShader : public IShader
 
 struct StylizedGouraudShader : public IShader
 {
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     Vec3f varying_intensity;      // 顶点着色器写入，片段着色器读取
 
     // iface为面的序号，nthvert为顶点序号
@@ -52,8 +54,8 @@ struct StylizedGouraudShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = Projection * ModelView * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 计算光照强度
         varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir);
         return gl_Vertex;
@@ -83,7 +85,7 @@ struct StylizedGouraudShader : public IShader
 
 struct TextureGouraudShader : public IShader
 {
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     Vec3f varying_intensity;      // 顶点着色器写入，片段着色器读取
     mat<2, 3, float> varying_uv;  // uv坐标，对应三个顶点
 
@@ -91,8 +93,8 @@ struct TextureGouraudShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = Projection * ModelView * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 从obj文件读取uv坐标
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
         // 计算光照强度
@@ -112,7 +114,7 @@ struct TextureGouraudShader : public IShader
 
 struct NormalMapShader : public IShader
 {
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     mat<2, 3, float> varying_uv;  // uv坐标，对应三个顶点
     mat<4, 4, float> uniform_M;   //  Projection*ModelView
     mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
@@ -121,8 +123,8 @@ struct NormalMapShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = uniform_M * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 从obj文件读取uv坐标
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
         return gl_Vertex;
@@ -132,7 +134,7 @@ struct NormalMapShader : public IShader
     {
         // 为当前像素计算uv坐标插值
         Vec2f uv = varying_uv * bar;
-        // 将法线转换到屏投影空间
+        // 将法线转换到投影空间
         Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
         // 将光源方向转换到投影空间
         Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
@@ -145,7 +147,7 @@ struct NormalMapShader : public IShader
 
 struct PhongShader : public IShader
 {
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     mat<2, 3, float> varying_uv;  // uv坐标，对应三个顶点
     mat<4, 4, float> uniform_M;   //  Projection*ModelView
     mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
@@ -154,8 +156,8 @@ struct PhongShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = uniform_M * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 从obj文件读取uv坐标
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
         return gl_Vertex;
@@ -165,7 +167,7 @@ struct PhongShader : public IShader
     {
         // 为当前像素计算uv坐标插值
         Vec2f uv = varying_uv * bar;
-        // 将法线转换到屏投影空间
+        // 将法线转换到投影空间
         Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
         // 将光源方向转换到投影空间
         Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
@@ -188,7 +190,7 @@ struct PhongShader : public IShader
 struct TangentNormalMapShader : public IShader
 {
     mat<2, 3, float> varying_uv;  // uv坐标，对应三个顶点
-    mat<4, 3, float> varying_tri; // 变换后的顶点坐标
+    mat<3, 3, float> varying_tri; // 变换后的顶点坐标
     mat<3, 3, float> varying_nrm; // 变换后的顶点法线
     mat<3, 3, float> ndc_tri;     // triangle in normalized device coordinates
     mat<4, 4, float> uniform_M;   //  Projection*ModelView
@@ -198,8 +200,8 @@ struct TangentNormalMapShader : public IShader
     {
         // 从.obj文件读取顶点坐标，并转换到裁剪空间
         Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-        gl_Vertex = uniform_M * gl_Vertex;
-        varying_tri.set_col(nthvert, gl_Vertex);
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
         // 从obj文件读取uv坐标
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
         // 从.obj文件读取顶点法线，并转换到裁剪空间
@@ -240,6 +242,73 @@ struct TangentNormalMapShader : public IShader
     }
 };
 
+struct ShadowShader : public IShader
+{
+    mat<4, 4, float> uniform_M;       //  Projection*ModelView
+    mat<4, 4, float> uniform_MIT;     // (Projection*ModelView).invert_transpose()
+    mat<4, 4, float> uniform_Mshadow; // transform framebuffer screen coordinates to shadowbuffer screen coordinates
+    mat<2, 3, float> varying_uv;      // triangle uv coordinates, written by the vertex shader, read by the fragment shader
+    mat<3, 3, float> varying_tri;     // triangle coordinates before Viewport transform, written by VS, read by FS
+
+    ShadowShader(Matrix M, Matrix MIT, Matrix MS) : uniform_M(M), uniform_MIT(MIT), uniform_Mshadow(MS), varying_uv(), varying_tri() {}
+
+    virtual Vec4f vertex(int iface, int nthvert)
+    {
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        Vec4f gl_Vertex = Viewport * Projection * ModelView * embed<4>(model->vert(iface, nthvert));
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
+        return gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color)
+    {
+        Vec4f sb_p = uniform_Mshadow * embed<4>(varying_tri * bar); // corresponding point in the shadow buffer
+        sb_p = sb_p / sb_p[3];
+        int idx = int(sb_p[0]) + int(sb_p[1]) * width;               // index in the shadowbuffer array
+        float shadow = .3 + .7 * (shadowbuffer[idx] < sb_p[2] + 2.); // magic coeff to avoid z-fighting
+        // 插值uv坐标
+        Vec2f uv = varying_uv * bar;
+        // 将法线转换到投影空间
+        Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+        // 将光源方向转换到投影空间
+        Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+        // 计算反射光线
+        Vec3f r = (n * (n * l * 2.f) - l).normalize();
+        // 计算高光强度系数
+        float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
+        // 计算漫反射强度系数
+        float diff = std::max(0.f, n * l);
+        // 读取漫反射纹理
+        TGAColor c = model->diffuse(uv);
+        // 着色
+        for (int i = 0; i < 3; i++)
+            color[i] = std::min<float>(20 + c[i] * shadow * (1.2 * diff + .6 * spec), 255);
+        return false;
+    }
+};
+
+struct DepthShader : public IShader
+{
+    mat<3, 3, float> varying_tri;
+
+    DepthShader() : varying_tri() {}
+
+    virtual Vec4f vertex(int iface, int nthvert)
+    {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));   // read the vertex from .obj file
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
+        return gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color)
+    {
+        Vec3f p = varying_tri * bar;
+        color = TGAColor(255, 255, 255) * (p.z / depth);
+        return false;
+    }
+};
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
@@ -247,51 +316,94 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    lookat(eye, center, up);
-    viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-    projection(-1.f / (eye - center).norm());
-    light_dir = proj<3>((Projection * ModelView * embed<4>(light_dir, 0.f))).normalize();
-
     float *zbuffer = new float[width * height];
-    for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max())
-        ;
-
-    TGAImage image(width, height, TGAImage::RGB);
-
-    // GouraudShader shader;
-
-    // StylizedGouraudShader shader;
-
-    // TextureGouraudShader shader;
-
-    // NormalMapShader shader;
-    // shader.uniform_M   =  Projection*ModelView;
-    // shader.uniform_MIT = (Projection*ModelView).invert_transpose();
-
-    PhongShader shader;
-    shader.uniform_M = Projection * ModelView;
-    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
-
-    // TangentNormalMapShader shader;
-    // shader.uniform_M   =  Projection*ModelView;
-    // shader.uniform_MIT = (Projection*ModelView).invert_transpose();
-
-    for (int i = 1; i < argc; i++)
+    for (int i = width * height; i--;)
     {
-        model = new Model(argv[i]);
-        for (int i = 0; i < model->nfaces(); i++)
+        zbuffer[i] = -std::numeric_limits<float>::max();
+    }
+    shadowbuffer = new float[width * height];
+
+    light_dir.normalize();
+
+    // 渲染阴影
+    {
+        TGAImage depth(width, height, TGAImage::RGB);
+        lookat(light_dir, center, up);
+        viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+        projection(0);
+
+        DepthShader depthshader;
+
+        for (int i = 1; i < argc; i++)
         {
-            for (int j = 0; j < 3; j++)
+            model = new Model(argv[i]);
+
+            Vec4f screen_coords[3];
+            for (int i = 0; i < model->nfaces(); i++)
             {
-                shader.vertex(i, j);
+                for (int j = 0; j < 3; j++)
+                {
+                    screen_coords[j] = depthshader.vertex(i, j);
+                }
+                triangle(screen_coords, depthshader, depth, shadowbuffer);
             }
-            triangle(shader.varying_tri, shader, image, zbuffer);
+            delete model;
         }
-        delete model;
+        depth.flip_vertically(); // to place the origin in the bottom left corner of the image
+        depth.write_tga_file("depth.tga");
     }
 
-    image.flip_vertically(); // 上下翻转，让原点在左下角
-    image.write_tga_file("output.tga");
+    Matrix M = Viewport * Projection * ModelView;
+
+    // 渲染图像
+    {
+        TGAImage image(width, height, TGAImage::RGB);
+
+        lookat(eye, center, up);
+        viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+        projection(-1.f / (eye - center).norm());
+
+        // GouraudShader shader;
+
+        // StylizedGouraudShader shader;
+
+        // TextureGouraudShader shader;
+
+        // NormalMapShader shader;
+        // shader.uniform_M   =  Projection*ModelView;
+        // shader.uniform_MIT = (Projection*ModelView).invert_transpose();
+
+        // PhongShader shader;
+        // shader.uniform_M = Projection * ModelView;
+        // shader.uniform_MIT = (Projection * ModelView).invert_transpose();
+
+        // TangentNormalMapShader shader;
+        // shader.uniform_M   =  Projection*ModelView;
+        // shader.uniform_MIT = (Projection*ModelView).invert_transpose();
+
+        ShadowShader shader(Projection * ModelView, (Projection * ModelView).invert_transpose(), M * (Viewport * Projection * ModelView).invert());
+
+        for (int i = 1; i < argc; i++)
+        {
+            model = new Model(argv[i]);
+
+            Vec4f screen_coords[3];
+            for (int i = 0; i < model->nfaces(); i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    screen_coords[j] = shader.vertex(i, j);
+                }
+                triangle(screen_coords, shader, image, zbuffer);
+            }
+            delete model;
+        }
+
+        image.flip_vertically(); // 上下翻转，让原点在左下角
+        image.write_tga_file("output.tga");
+    }
+
     delete[] zbuffer;
+    delete[] shadowbuffer;
     return 0;
 }
